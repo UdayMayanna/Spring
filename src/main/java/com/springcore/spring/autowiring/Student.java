b652efe1b33ef0968bcd131b1869f74c7608d6fb
USER <USER@Uday5961>
2025-07-10 10:42:35 +0530

    Codes for autowiring using annotation @Autowired and use of @Qualifier

--- src/main/java/com/springcore/spring/autowiring/Student.java
+++ src/main/java/com/springcore/spring/autowiring/Student.java
@@ -1,15 +1,23 @@
 package com.springcore.spring.autowiring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Student {
+	
+  //@Autowired
+  //-> Autowiring using property
    private Course course;
 
 public Student() {
 	super();
 	// TODO Auto-generated constructor stub
 }
-
-public Student(Course course) {
+@Autowired  
+//-> Autowiring using constructor
+public Student(@Qualifier("course2") Course course) {
 	super();
+	System.out.println("Constructor is called");
 	this.course = course;
 }
 
@@ -17,8 +25,11 @@ public Course getCourse() {
 	return course;
 }
 
+//@Autowired
+//-> Autowiring using stter method
 public void setCourse(Course course) {
 	this.course = course;
+	System.out.println("Setting Value");
 }
 
 @Override
